0d6202e3d7481ba4a62fd9e8c2833bf10c0ce199
Wisdom Ossai <ossaiwisdom48@gmail.com>
2018-08-16 12:24:47 +0100

    Add package-lock.json file

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const http = require('http');
+const url = require('url');
 
 const host = 'localhost';
-const port = 8000;
+const port = 8080;
 
+const page = url.parse(req.url).pathname;
+console.log(page);
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World \n');
+    res.setHeader('Content-Type', 'text/html');
+    res.end();
 });
 
 server.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}/`);
 });
+
